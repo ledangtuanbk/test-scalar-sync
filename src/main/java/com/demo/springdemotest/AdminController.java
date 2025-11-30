bcd9454fd0c685843152ed3a535beb31a80ffdbf
Tommy <ledangtuanbk@gmail.com>
2025-11-30 20:12:37 +0700

    fix

--- src/main/java/com/demo/springdemotest/AdminController.java
+++ src/main/java/com/demo/springdemotest/AdminController.java
@@ -1,5 +1,7 @@
 package com.demo.springdemotest;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
-@HiddenForProfiles({"prod", "production"})
+@HiddenForProfiles({"dev"})
+@Tag(name = "admin")
 public class AdminController {
+
 
     @GetMapping("/status")
     public ResponseEntity<String> getStatus() {
